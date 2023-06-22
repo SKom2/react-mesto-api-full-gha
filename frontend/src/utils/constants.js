da7d6a8a5c7babf0249686d6a46b66a25c40a861
Alex Spark <san4es.komgmail.com>
2023-06-22 19:49:28 +0300

    feat new api

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const apiConfig = {
-    url: 'https://nomoreparties.co/v1/cohort-60',
+    url: 'http://api.mesto-otdiha.nomoredomains.work',
     headers: {
         authorization: '0823f3d8-cee4-4f0b-9f88-c1dcbe025215',
         'Content-Type': 'application/json'
