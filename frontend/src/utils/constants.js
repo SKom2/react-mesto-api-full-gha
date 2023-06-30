c224542855f66baf204a188c16ce410a0ef53c16
Alex Spark <san4es.komgmail.com>
2023-06-30 10:43:38 +0300

    fix mixed content

--- frontend/src/utils/constants.js
+++ frontend/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const apiConfig = {
-    url: 'http://api.mesto-otdiha.nomoredomains.work',
+    url: 'https://api.mesto-otdiha.nomoredomains.work',
     headers: {
         Authorization: `Bearer ${localStorage.getItem('jwt')}`,
         'Content-Type': 'application/json',
